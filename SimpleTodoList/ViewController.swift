01387435ec1e529ff55bb0a6008eda5c64a695ea
meg-kuma <syou.canopus.6104@gmail.com>
2017-04-30 21:40:09 +0900

    Initial Commit

---
+++ SimpleTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTodoList
+//
+//  Created by 目黒将稔 on 2017/04/30.
+//  Copyright © 2017年 Masatoshi Meguro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
